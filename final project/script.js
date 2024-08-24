1fb2443937930f13b35a6ad3bd239634ee47ec7d
Saleh14Sherif <162384783+Saleh14Sherif@users.noreply.github.com>
2024-08-25 00:04:04 +0300

    Add files via upload

---
+++ final project/script.js
@@ -0,0 +1,6 @@
+function ShowName(photo){
+    document.getElementById("label").innerHTML=photo.alt ;
+}
+function ShowLastName(photo){
+    document.getElementById("label").innerHTML="last seleceted :" + photo.alt;
+}
